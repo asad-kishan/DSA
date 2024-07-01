54c806c5ac27688d7e9a22ea7bd895fb07244986
Asaduzzaman Kishan <asaduzzamankishan2@gmail.com>
2024-07-01 11:26:50 +0600

    first commit

---
+++ sorting algorithms/selection_sort.cpp
@@ -0,0 +1,43 @@
+// Best-case time complexity: O(n²)
+// Worst-case time complexity: O(n²)
+// In-place sorting algorithm
+// Space complexity: O(1)
+
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+void selection_sort(vector<int>& arr) {
+    int n = arr.size();
+
+    for(int i = 0; i < n-1; i++) {
+        int minIdx = i;
+
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+
+        swap(arr[i], arr[minIdx]);
+    }
+}
+
+void print_array(const vector<int>& arr) {
+    for(int num : arr)
+        cout << num << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+
+    cout << "Original Array: ";
+    print_array(arr);
+
+    selection_sort(arr);
+    cout << "Sorted Array: ";
+    print_array(arr);
+
+    return 0;
+}
